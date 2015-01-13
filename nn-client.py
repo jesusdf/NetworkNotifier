86385a8455389315326e9d70fe34c6ce241c0bc0
JesusDF <jesusdf@gmail.com>
2015-01-13 03:03:56 +0100

    Fix encoding in python comment.

--- nn-client.py
+++ nn-client.py
@@ -4,7 +4,7 @@
 # Network Notifier #
 ####################
 
-#Copyright (C) 2015 Jesús Diéguez Fernández
+#Copyright (C) 2015 Jesus Dieguez Fernandez
 
 #This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
